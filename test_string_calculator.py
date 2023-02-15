87c6970a99735839029ec5253293404381a634b2
yujhenchen <yujhen.chen93@gmail.com>
2023-02-15 15:31:14 +0800

    add fail test

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -39,6 +39,9 @@ class TestStringCalculator(unittest.TestCase):
     def test_postfix_return_1(self) -> None:
         self.assertEqual(1, self.stringCalculator.add("1,"))
 
+    def test_two_numbers_return_5(self) -> None:
+        self.assertEqual(5, self.stringCalculator.add("2,3"))
+
 
 if __name__ == "__main__":
     unittest.main()
